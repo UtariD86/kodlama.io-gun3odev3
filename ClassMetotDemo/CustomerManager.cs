dc7217523991eaae37f8536949b2fbf42b5efc98
Mehmet Göksu <69503659+UtariD86@users.noreply.github.com>
2022-05-20 19:49:40 +0300

    Add files via upload

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        
+            Customer customer;
+            List<Customer> _customers;
+            public CustomerManager()
+            {
+                _customers = new List<Customer>()
+            {
+                new Customer() { Id = 1, Name ="Ahmet",Surname="Çiftçi",Money=1500},
+                new Customer() { Id = 2, Name ="Mehmet",Surname="Göksu",Money =1800}
+            };
+            }
+
+            public void Add(Customer customer)
+            {
+                _customers.Add(customer);
+            }
+            public void Delete(Customer customer)
+            {
+                var dltCustomer = _customers.SingleOrDefault(c => c.Name == customer.Name);
+                _customers.Remove(dltCustomer);
+            }
+            public List<Customer> GetAll()
+            {
+                return _customers;
+            }
+        
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ödev şimdilik eksiktir. Silme Fonksiyonu eklenmedi. 
+            //Eklemede de küçük bir hata var daha sonra düzeltmek
+            //üzere şimdilik es geçiyorum.
+            Customer addCustomer = new Customer();
+            CustomerManager customerManager = new CustomerManager();
+            string choice = "a";
+            while (choice != "4")
+            {
+                
+                Console.WriteLine("Lütfen yapmak istediğiniz işlemi tuşlayınız. \n" +
+                                  "1) Müşteri Ekle \n" +
+                                  "2) Müşteri Listele \n" +
+                                  "3) Müşteri Sil \n" +
+                                  "4) Programı Sonlandır");
+                choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    Console.WriteLine("Müşteri Id:");
+                    addCustomer.Id = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Müşteri Adı:");
+                    addCustomer.Name = Console.ReadLine();
+
+                    Console.WriteLine("Müşteri Soyadı:");
+                    addCustomer.Surname = Console.ReadLine();
+
+                    Console.WriteLine("Müşteri Hesap Bakiyesi:");
+                    addCustomer.Money = decimal.Parse(Console.ReadLine());
+                    customerManager.Add(addCustomer);
+                }
+                if (choice == "2")
+                {
+                    Console.WriteLine("|   Müşteri Id   |   Müşteri Adı  | Müşteri Soyadı |Müşteri Bakiyesi|");
+                    foreach (var customer in customerManager.GetAll())
+                    {
+                        Console.WriteLine("|       " + customer.Id + "       " +
+                                          "|   " + customer.Name + "   " +
+                                          "|   " + customer.Surname + "   " +
+                                          "|   " + customer.Money + "   ");
+                    }
+                }
+                if (choice == "3")
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("Lütfen geçerli bir seçim yapınız");
+                }
+            }
+        }
+    }
+}
